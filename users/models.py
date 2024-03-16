0438264716b3996113b67953801f3e649e4a3733
kelvin wepo <kelvinwepo7710@gmail.com>
2024-03-16 23:54:52 +0300

    this commit modifies models.py

--- users/models.py
+++ users/models.py
@@ -1,25 +1,36 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-from .managers import CustomUserManager
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-# Create your models here.
+        return self.create_user(email, password, **extra_fields)
 
-class CustomUser(AbstractUser):
-      username =None
-      email = models.EmailField(_('email address'), max_length=100, null=True, unique=True)
-      phone = models.CharField(max_length=15, null=True)
-      first_name = models.CharField(max_length=100, null=True)
-      last_name = models.CharField(max_length=100, null=True)
-      last_login = models.DateTimeField(auto_now_add=True, null=True)    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now, editable=False, null=True)
 
-      USERNAME_FIELD = 'email'
-      REQUIRED_FIELDS = []
+    objects = CustomUserManager()
 
-      objects = CustomUserManager()
-      
-      def __str__(self): 
-        return "{}".format(self.email)             
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
+    def __str__(self):
+        return self.email
