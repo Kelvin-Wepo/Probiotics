ceb0398d1af81d1f3bc5099e571ce702669c9a8c
kelvin wepo <kelvinwepo7710@gmail.com>
2024-03-18 23:49:46 +0300

    this commit modifies urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from django.conf.urls import include
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'users'
 
-
 urlpatterns = [
-    path('sign_up/', views.SignUp.as_view(), name='sign_up'),
- #   path('login/', views.login, name='login'),
-    path('', include("django.contrib.auth.urls")),
+    path('sign_up/', views.signup, name='sign_up'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
